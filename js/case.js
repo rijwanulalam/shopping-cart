6955b3c3049fc0b0e0df7ec806810d977d98c26d
rijwanulalam <rijwanul007@gmail.com>
2023-11-15 13:04:07 +0600

    case calcultaion added

--- js/case.js
+++ js/case.js
@@ -1,14 +1,34 @@
-document.getElementById("case-number-plus").addEventListener("click", function(){
-    const caseInputField = document.getElementById("case-number-field");
+function increaseDecrease(inputId, isIncrease){
+    const caseInputField = document.getElementById(inputId);
     const caseNumber = parseInt(caseInputField.value);
-    const newCaseNumber = caseNumber + 1;
+
+    let newCaseNumber;
+    if(isIncrease){
+        newCaseNumber = caseNumber + 1;
+    }
+    else{
+        newCaseNumber = caseNumber - 1;
+    }
     caseInputField.value = newCaseNumber;
+    return newCaseNumber;
+}
+
+
+function calculatePrice(priceId ,newCaseNumber){
+    const casePrice = document.getElementById(priceId);
+    const newCasePrice = newCaseNumber * 59;
+    casePrice.innerText = newCasePrice;
+}
+
+
+document.getElementById("case-number-plus").addEventListener("click", function(){
     
+    const newCaseNumber =  increaseDecrease("case-number-field",true);
+    calculatePrice("case-price", newCaseNumber);
 })
 
 document.getElementById("case-number-minus").addEventListener("click", function(){
-    const caseInputField = document.getElementById("case-number-field");
-    const caseNumber = caseInputField.value;
-    const newCaseNumber = caseNumber - 1;
-    caseInputField.value = newCaseNumber;
+    
+    const newCaseNumber =  increaseDecrease("case-number-field",false);
+    calculatePrice("case-price", newCaseNumber);
 })
